41d8df7386ba8edf8d4e49a43cef18050016b1c2
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-15 22:11:54 +0300

    ft:added configuration classes

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+class Config:
+    '''
+    general configuration
+    '''
+    
+    @staticmethod
+    def init_app(app):
+        pass
+    
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    '''
+    
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    '''
+    DEBUG= True
+    
+config_options ={
+    'development':DevConfig,
+    'production':ProdConfig
+}
