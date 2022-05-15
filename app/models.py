44dc8e0374d1a662553c0f701c681b8e02b92a96
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-15 22:43:18 +0300

    ft:created a user class

--- app/models.py
+++ app/models.py
@@ -11,4 +11,25 @@ class Quote:
     def __init__(self,author,quote):
         self.author = author
         self.quote = quote
+
+
+class User(UserMixin):
+    
+    
+    @property
+    def password(self):
+        raise AttributeError("Unable to read this")
+    
+    @password.setter
+    def password(self,password):
+        self.hash_pass = generate_password_hash(password)
+        
+    def  set_password(self,password):
+        self.hash_pass = generate_password_hash(password)
+        
+    def verify_password(self,password):
+        self.hash__pass = check_password_hash(password)
+        
+    
+        
         
