21df938d2f01abcc1297d48e0b8b25ae0dc27771
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-16 20:06:16 +0300

    ft:created adatabase table for posts

--- app/models.py
+++ app/models.py
@@ -50,6 +50,17 @@ class User(UserMixin, db.Model):
     
     
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
+    content = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    
+    
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 
 
