b32d6ead889705ec034c1c6b6c1b1a88cacb4baf
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-16 20:57:25 +0300

    ft:added manager.shell to enable us access and update the database

--- manage.py
+++ manage.py
@@ -1,13 +1,22 @@
 from flask_script import Manager,Server
-from app import create_app
+from app import create_app,db
+from app.models import User, Post
 
 
-app = create_app('development')
+
+app = create_app('production')
 
 
 manager = Manager(app)
 manager.add_command('server', Server)
+manager.add_command('db')
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Post = Post)
 
 
 if __name__ == '__main__':
     manager.run()
+    db.create_all()
